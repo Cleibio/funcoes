e8250a0884cb419bae73dab87caa33384d63b652
Cleibio <88244996+Cleibio@users.noreply.github.com>
2022-05-16 13:26:35 -0300

    Update sequencia_crescente.php

--- sequencia_crescente.php
+++ sequencia_crescente.php
@@ -1,49 +1,25 @@
 <?php
-function sequencia_crescente($array){
-    $contador_externo = 0;
-    $contador_interno = 0;
-    $repeticao = 0;
-    $novo = array();
-    $result = true;
-    while($contador_externo < count($array)){
-        while($contador_interno < count($array) ){
-            if($array[$contador_externo] == $array[$contador_interno]){
-                $repeticao = $repeticao + 1;
-            }
-            $contador_interno++;
+function teste($array){
+    $contador = 0;
+    $proximo_indice = 1;
+    $desordem = 0;
+    //Se desordem for maoir que 1, não há mais necessidade de continuar percorrendo o array.
+    while($contador < count($array)-1 && $desordem < 2){
+        if($array[$contador] >= $array[$proximo_indice]){
+            $desordem += 1;
         }
-        if($repeticao > 1){
-            $reseult = false;
-        }
-        $contador_externo++;
-        $contador_interno = 0;
-        $repeticao = 0;
+        $contador++;
+        $proximo_indice++;
     }
 
-    return $result;
-}
-    $array_sequencial = [8,7,5,4,3];
-    $result = sequencia_crescente($array_sequencial);
-    var_dump($result);
-    $ordem = false;
-	for ($i = 0; $i < count($array_sequencial); $i++){
-        if($array_sequencial[$i] <= $array_sequencial[$i++]){
-			$ordem = true;
-		}else{
-			$ordem = false;
-		}
-	}
-    var_dump($ordem);
-
-    if($result == true && $ordem == true){
-        $result = 'falso';
+    if($desordem > 1){
+        return false;
     }else{
-        $result = 'verdadeiro';
+        return true;
     }
+}
 
-    $exibir = "[";
-    foreach($array_sequencial as $sequencial){
-        $exibir .= $sequencial ." ";
-    }
-    print_r("$exibir] = $result \n");
+$array = [2,3,4,5,1];
+$sequencia = teste($array);
+print($sequencia == true ? "Verdadeiro" : "Falso");
 ?>
